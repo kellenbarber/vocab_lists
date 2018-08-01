c2847d8e898ee065b8f86b7c6df08cf7a9bbb4aa
kellenbarber <kellenbarber@gmail.com>
2018-07-31 20:31:52 -0700

    got smart and doing this with the html copied from the source so I don't have to do some weird character conversion; installed some xpath stuff

--- convert.js
+++ convert.js
@@ -1,4 +1,5 @@
-const xml2js = require('xml2js');
+const xpath = require('xpath');
+const dom = require('xmldom').DOMParser;
 const fs = require('fs');
 
 // first argument is the perseus xml file
@@ -6,49 +7,11 @@ const inputPath = process.argv[2];
 // second is the output file to create/write to
 const outputPath = process.argv[3];
 
-const charMap = {
-    'a': 'α',
-    'b': 'β',
-    'c': 'ξ',
-    'd': 'δ',
-    'e': 'ε',
-    'f': 'φ', 
-    'g': 'γ',
-    'h': 'η',
-    'i': 'ι',
-    'k': 'κ',
-    'l': 'λ',
-    'm': 'μ',
-    'n': 'ν',
-    'o': 'ο',
-    'p': 'π',
-    'q': 'θ',
-    'r': 'ρ',
-    's': 'σ',
-    't': 'τ',
-    'u': 'υ',
-    'w': 'ω',
-    'x': 'χ',
-    'y': 'ψ',
-    'z': 'ζ',
-    '/': ''
-};
-
 fs.readFile(inputPath, 'utf8', (err, data) => {
     if (err) {
         console.log(err);
-        return;
     }
 
-    const parser = new xml2js.Parser();
-    parser.parseString(data, (err, result) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-
-        Object.entries(result.frequencies).forEach(freq => {
-            console.log(freq);
-        });
-    });
+    const doc = new dom().parseFromString(data);
+    console.log(doc);
 });
